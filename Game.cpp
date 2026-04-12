b6eb029ed3334220fbef29d20f46326aa8cb1fa4
cristinac-droid <cristina.crusoveanu@gmail.com>
2026-04-12 16:33:17 +0000

    changed main menu

--- Game.cpp
+++ Game.cpp
@@ -61,57 +61,25 @@ NPC (
 
 int main () {
     Player* playermain = new Player ("PlayerMain", 35, HUMAN, FEMALE);
-    return 0;
 
-int choice;
 
-    do {
-        cout << "\n=== GAME MENU ===\n";
-        cout << "1. Play\n";
-        cout << "2. Options\n";
-        cout << "3. Exit\n";
-        cout << "Choose: ";
-        cin >> choice;
+	int choice;
+	while(1) {
+		cout << "=== GAME MENU ===" << endl;
+		cout << "1. Play" << endl;
+		cout << "2. Exit" << endl;
+		cout << "Choose: ";
+		cin >> choice;
 
-        switch (choice) {
-            case 1:
-                cout << "Starting game...\n";
-                break;
-
-            case 2: {
-                int difficulty;
-                cout << "\n-- Options --\n";
-                cout << "1. Easy\n";
-                cout << "2. Medium\n";
-                cout << "3. Hard\n";
-                cout << "Choose difficulty: ";
-                cin >> difficulty;
-
-                switch (difficulty) {
-                    case 1:
-                        cout << "Easy selected\n";
-                        break;
-                    case 2:
-                        cout << "Medium selected\n";
-                        break;
-                    case 3:
-                        cout << "Hard selected\n";
-                        break;
-                    default:
-                        cout << "Invalid difficulty\n";
-                }
-                break;
-            }
-
-            case 3:
-                cout << "Exiting game...\n";
-                break;
-
-            default:
-                cout << "Invalid choice\n";
-        }
-
-    } while (choice != 3);
-
-    return 0;
+		if (choice == 2) {
+			return 0;
+		}
+		else if (choice == 1) {
+			break;
+		}
+		else {
+			cout << "Invalid choice." << endl;
+		}
+	}
+	return 0;
 }
